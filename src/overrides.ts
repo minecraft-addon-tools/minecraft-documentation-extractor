a4531669265353dd5c1134389152281905c6bc77
Steven Blom <atomicblom@binaryvibrance.net>
2019-05-30 10:10:29 +0930

    Add missing copyright notice to overrides

--- src/overrides.ts
+++ src/overrides.ts
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2018 Filip Hejsek, Steven Blom
+ *
+ * This file is part of minecraft-documentation-extractor.
+ *
+ * minecraft-documentation-extractor is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * minecraft-documentation-extractor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with minecraft-documentation-extractor.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import * as untypedOverrideList from "./type-overrides.json";
 import { cloneDeep } from "lodash";
 import { isArray, isObject } from "util";
